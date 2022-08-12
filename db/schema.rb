6fc53691ca457d7268de90cbc873163ad6e77a87
Andre Bauer <andrebauer@live.com>
2022-08-12 11:44:39 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_030124) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
     t.integer "rating"
     t.datetime "created_at", null: false
